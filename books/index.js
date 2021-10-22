675a86e37d14c1ac7a7ca45552562ccfec6e8107
Sami Kuikka <sami.kuikka@aalto.fi>
2021-10-22 12:34:29 +0300

    8.3

--- books/index.js
+++ books/index.js
@@ -93,6 +93,7 @@ const typeDefs = gql`
       name: String!
       id: ID!
       born: String
+      bookCount: Int!
   }
 
   type Book {
@@ -107,6 +108,7 @@ const typeDefs = gql`
       bookCount: Int!
       authorCount: Int!
       allBooks: [Book!]!
+      allAuthors: [Author!]!
   }
 `
 
@@ -114,7 +116,11 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books
+      allBooks: () => books,
+      allAuthors: () => authors
+  },
+  Author: {
+      bookCount: (root) => books.filter(book => book.author === root.name).length
   }
 }
 
