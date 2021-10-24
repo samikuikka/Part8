65925f057970cdb2da7aca64a785462acd2a466d
Sami Kuikka <sami.kuikka@aalto.fi>
2021-10-24 11:02:45 +0300

    8.16

--- books/index.js
+++ books/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
 const { v4 } = require('uuid')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
 const Author = require('./models/author')
 const Book = require('./models/book')
+const User = require('./models/user')
 
+const JWT_SECRET = process.env.SECRET
 const MONGODB_URI = process.env.MONGODB_URI
 
 console.log('connecting to', MONGODB_URI)
@@ -19,6 +22,17 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
 })
 
 const typeDefs = gql`
+
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Author {
       name: String!
       id: ID!
@@ -39,6 +53,7 @@ const typeDefs = gql`
       authorCount: Int!
       allBooks(author: String, genre: String): [Book!]!
       allAuthors: [Author!]!
+      me: User
   }
 
   type Mutation {
@@ -52,6 +67,14 @@ const typeDefs = gql`
           name: String!
           setBornTo: Int!
       ): Author
+      createUser(
+        username: String!
+        favoriteGenre: String!
+      ): User
+      login(
+        username: String!
+        password: String!
+      ): Token
   }
 `
 
@@ -85,6 +108,10 @@ const resolvers = {
       },
       allAuthors: async () =>  {
         return await Author.find({})
+      },
+      me: (root, args, context) => {
+        console.log(context)
+        return context.currentUser
       }
   },
   Author: {
@@ -97,8 +124,13 @@ const resolvers = {
       }
   },
   Mutation: {
-      addBook: async (root, args) => {
+      addBook: async (root, args, context) => {
           const book = new Book({ ...args })
+          const currentUser = context.currentUser
+
+          if (!currentUser) {
+            throw new AuthenticationError("not authenticated")
+          }
 
           //args validation
           if(args.title.length < 4) {
@@ -132,7 +164,12 @@ const resolvers = {
             })
           }
       },
-      editAuthor: async (root, args) => {
+      editAuthor: async (root, args, context) => {
+          const currentUser = context.currentUser
+
+          if (!currentUser) {
+            throw new AuthenticationError("not authenticated")
+          }
           const author = await Author.findOneAndUpdate({ name: args.name }, { born: args.setBornTo}, { new: true})
             .catch( (error) => {
               throw new UserInputError(error.message, {
@@ -140,13 +177,49 @@ const resolvers = {
               })
             })
           return author
-      }
+      },
+
+      createUser: (root, args) => {
+        const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+    
+        return user.save()
+          .catch(error => {
+            throw new UserInputError(error.message, {
+              invalidArgs: args,
+            })
+          })
+      },
+
+      login: async (root, args) => {
+        const user = await User.findOne({ username: args.username })
+    
+        if ( !user || args.password !== 'secret' ) {
+          throw new UserInputError("wrong credentials")
+        }
+    
+        const userForToken = {
+          username: user.username,
+          id: user._id,
+        }
+    
+        return { value: jwt.sign(userForToken, JWT_SECRET) }
+      },
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id).populate('friends')
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
